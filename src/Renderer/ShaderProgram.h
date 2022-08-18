23473338a79fd351fe2a011ac2117f07b4e3430e
Aleksey <medvedev_a_l@mail.ru>
2022-08-19 04:43:08 +0500

    added transformation matrix

--- src/Renderer/ShaderProgram.h
+++ src/Renderer/ShaderProgram.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <glad/glad.h>
 #include <string>
+#include <glm/mat4x4.hpp>
 
 namespace Renderer
 {
@@ -18,6 +19,7 @@ namespace Renderer
             bool isCompiled()const {return m_isCompiled;}
             void use()const;
             void setInt(const std::string& name, const GLint value);
+            void setMatrix4(const std::string& name, const glm::mat4& matrix);
 
         private:
             bool createShader(const std::string& source, const GLenum shaderType, GLuint& shaderID);
