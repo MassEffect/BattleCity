8342938db8d17440a65938e624060cebb74bde06
Aleksey <medvedev_a_l@mail.ru>
2022-08-22 02:43:54 +0500

    added AnimatedSprite class

--- src/Resources/ResourceManager.h
+++ src/Resources/ResourceManager.h
@@ -9,6 +9,7 @@ namespace Renderer
     class ShaderProgram;
     class Texture2D;
     class Sprite;
+    class AnimatedSprite;
 }
 
 class ResourceManager
@@ -37,9 +38,18 @@ class ResourceManager
 
         std::shared_ptr<Renderer::Sprite> getSprite(const std::string& spriteName);
 
-        std::shared_ptr<Renderer::Texture2D> loadTextureAtlas(const std::string textureName,
-                                                              const std::string texturePath,
-                                                              const std::vector<std::string> subTextures,
+        std::shared_ptr<Renderer::AnimatedSprite> loadAnimatedSprite(const std::string& spriteName,
+                                                                     const std::string& textureName,
+                                                                     const std::string& shaderName,
+                                                                     const unsigned int spriteWidth,
+                                                                     const unsigned int spriteHeight,
+                                                                     const std::string& subTextureName = "default");
+
+        std::shared_ptr<Renderer::AnimatedSprite> getAnimatedSprite(const std::string& spriteName);
+
+        std::shared_ptr<Renderer::Texture2D> loadTextureAtlas(std::string textureName,
+                                                              std::string texturePath,
+                                                              std::vector<std::string> subTextures,
                                                               const unsigned int subTextureWidth,
                                                               const unsigned int subTextureHeight);
 
@@ -54,6 +64,9 @@ class ResourceManager
 
         typedef std::map<const std::string, std::shared_ptr<Renderer::Sprite>> SpritesMap;
         SpritesMap m_sprites;
+
+        typedef std::map<const std::string, std::shared_ptr<Renderer::AnimatedSprite>> AnimatedSpritesMap;
+        AnimatedSpritesMap m_animatedSprites;
 
 
 
